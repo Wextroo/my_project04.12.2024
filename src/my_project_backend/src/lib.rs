6fe5b8ba979026c8b2d733d8d5ecfc368fe44a5f
Wojciech Rakoczy <rakoczywojtek715@gmail.com>
2024-12-04 09:40:41 +0000

    nieweim

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,19 @@
+use std::cell::RefCell;
+
+thread_local{
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+    MSG.with(|msg|{
+        msg.borrow.mut()
+    })
+}
+
+
+#[ic_cdk::query]
+fn get_msg() -> String {
+    msg.with(|msg| msg.borrow().clone())
 }
